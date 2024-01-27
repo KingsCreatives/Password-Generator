90e655d1ae1088fafc9be42c936499a217c8d5ae
Kwame Cody <kwamecody@gmail.com>
2024-01-27 18:59:42 +0000

    generate password and copy to clipboard functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect, useCallback, useRef} from 'react'
 import './App.css'
 
 function App() {
-  
+  const [length, useLength] = useState(8)
+  const [addNums, useAddNums] = useState(false)
+  const [specialChar, useSpecialChar] = useState(false)
+  const [password, usePassword] = useState("")
+  const passwordRef = useRef(null)
+
+  const generatePassword = useCallback(() => {
+    let generatedPassword = ''
+    let alphabets = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    let nums = "1234567890"
+    let char = "!@#$%^&*()_-+[]{}"
+
+    if(addNums) alphabets += nums
+    if(specialChar) alphabets += char
+
+    for(let i = 1; i < length; i++){
+      let charIndex = Math.floor(Math.random() * alphabets.length +  1)
+      generatedPassword += alphabets.charAt(charIndex)
+    }
+
+    usePassword(generatedPassword)
+  }, [length, addNums, specialChar])
+
+  const copyPassword = () => {
+    window.navigator.clipboard.writeText(password)
+    passwordRef.current?.select()
+  }
+
+  useEffect(() => {
+    generatePassword()
+  }, [length, addNums, specialChar])
   
 
   return (
@@ -15,12 +43,17 @@ function App() {
               <div className="w-full mx-1">
                   <input type="text"
                   className="w-full px-2 shadow-lg rounded-md py-1 outline-none"
-                   placeholder="Password" 
+                   placeholder="Password"
+                   readOnly
+                   value={password} 
+                   ref={passwordRef}
                   />
               </div>
               <div className="">
                   <button 
-                  className="bg-blue-700 shadow-lg rounded-md  px-2 py-1 text-white outline-none">
+                  className="bg-blue-700 shadow-lg rounded-md  px-2 py-1 text-white outline-none"
+                  onClick={copyPassword}
+                  >
                     Copy
                   </button>
               </div>
@@ -32,17 +65,25 @@ function App() {
                    min={8}
                    max={30} 
                    className="mx-2"
+                   value={length}
+                   onChange={e => useLength(e.target.value)}
                   />
-                  <label htmlFor="length">Length: 8</label>
+                  <label htmlFor="length">Length: {length}</label>
               </div>
               <div className="flex gap-3">
                  <div className="flex gap-1">  
-                  <input type="checkbox" name="numbers"/>
+                  <input type="checkbox" name="numbers"
+                  defaultChecked = {addNums}
+                   onChange={() => useAddNums((bool) => !bool)}
+                  />
                   <label htmlFor="numbers">Numbers</label>
                  </div>
 
                  <div className="flex gap-1">  
-                  <input type="checkbox" name="numbers"/>
+                  <input type="checkbox" name="numbers"
+                  onChange={() => useSpecialChar((bool => !bool))}
+                  defaultChecked = {specialChar}
+                  />
                   <label htmlFor="numbers">Char</label>
                  </div>
               </div>
